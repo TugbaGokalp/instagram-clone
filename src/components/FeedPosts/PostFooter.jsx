fb29933ebaeb3878e9550d95c20993550b912c48
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-04 12:37:17 +0300

    like - unlike post event added

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,5 +1,29 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import { useState } from "react";
+
 const PostFooter = () => {
-  return <>PostFooter</>;
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleClick = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box onClick={handleClick} cursor={"pointer"} fontSize={18}>
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default PostFooter;
