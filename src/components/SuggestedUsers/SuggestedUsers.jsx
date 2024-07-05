89e5f5eb2a781307ba3615006be7054b9e3d0628
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-05 17:48:51 +0300

    "Suggested for you and See all" row added

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,24 @@
-import { VStack } from "@chakra-ui/react";
+import { Flex, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={"bold"}
+          color={"gray.500"}
+          _hover={{ color: "gray.400" }}
+          cursor={"pointer"}
+        >
+          See all
+        </Text>
+      </Flex>
     </VStack>
   );
 };
