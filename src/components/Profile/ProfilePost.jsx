9a0b24391d11b54b0ecb507de91a87a124dd023c
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-10 18:16:32 +0300

    post image element added with prop

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,8 +1,8 @@
-import { Flex, GridItem, Text } from "@chakra-ui/react";
+import { Flex, GridItem, Image, Text } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 
-const ProfilePost = () => {
+const ProfilePost = ({ img }) => {
   return (
     <GridItem
       cursor={"pointer"}
@@ -42,6 +42,13 @@ const ProfilePost = () => {
           </Flex>
         </Flex>
       </Flex>
+      <Image
+        src={img}
+        alt="profile post"
+        w={"100%"}
+        h={"100%"}
+        objectFit={"cover"}
+      />
     </GridItem>
   );
 };
