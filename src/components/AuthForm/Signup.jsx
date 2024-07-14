d6a15a08e4afca12092af0e2ed514dced67e8158
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-14 09:09:27 +0300

    Creating a new user and adding it to the database

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,5 +1,12 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import useSignUpWithEmailAndPassword from "../../hooks/useSignUpWithEmailAndPassword";
 
@@ -62,11 +69,19 @@ const Signup = () => {
         </InputRightElement>
       </InputGroup>
 
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+
       <Button
         w={"full"}
         colorScheme="blue"
         size={"sm"}
         fontSize={14}
+        isLoading={loading}
         onClick={() => signup(inputs)}
       >
         Sign up
