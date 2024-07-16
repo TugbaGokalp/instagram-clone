dcaa5461a6adedef59280be34d3236aed72b4024
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-16 19:47:35 +0300

    Added useCallback to prevent infinite loop

--- src/hooks/useShowToast.js
+++ src/hooks/useShowToast.js
@@ -1,17 +1,22 @@
 import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 const useShowToast = () => {
   const toast = useToast();
 
-  const showToast = (title, description, status) => {
-    toast({
-      title,
-      description,
-      status,
-      duration: 5000,
-      isClosable: true,
-    });
-  };
+  // useCallback is used to prevent infinite loop, by catching the function
+  const showToast = useCallback(
+    (title, description, status) => {
+      toast({
+        title,
+        description,
+        status,
+        duration: 5000,
+        isClosable: true,
+      });
+    },
+    [toast]
+  );
   return showToast;
 };
 
