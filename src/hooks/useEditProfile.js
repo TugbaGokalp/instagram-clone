5fa55c8a69fcb92e169027d681de6519af861aef
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-19 12:55:09 +0300

    Second step for useEditProfile hook

--- src/hooks/useEditProfile.js
+++ src/hooks/useEditProfile.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import useAuthStore from "../store/authStore";
+import useUserProfileStore from "../store/userProfileStore";
 import useShowToast from "./useShowToast";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { firestore, storage } from "../firebase/storage";
-import { doc } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 
 const useEditProfile = () => {
   const [isUpdating, setIsUpdating] = useState(false);
   const authUser = useAuthStore((state) => state.user);
+  const setAuthUser = useAuthStore((state) => state.setUser);
+  const setUserProfile = useUserProfileStore((state) => state.user);
   const showToast = useShowToast();
 
   const editProfile = async (inputs, selectedFile) => {
@@ -24,6 +27,19 @@ const useEditProfile = () => {
         await uploadString(storageRef, selectedFile, "data_url");
         URL = await getDownloadURL(ref(storage, "profilePics/${authUser.uid"));
       }
+
+      const updatedUser = {
+        ...authUser,
+        fullName: inputs.fullName || authUser.fullName,
+        username: inputs.username || authUser.username,
+        bio: inputs.bio || authUser.bio,
+        profilePicURL: URL || authUser.profilePicURL,
+      };
+
+      await updateDoc(userDocRef, updatedUser);
+      localStorage.setItem("user-info", JSON.stringify(updatedUser));
+      setAuthUser(updatedUser);
+      setUserProfile(updatedUser);
     } catch (error) {
       showToast("Error", error.message, "error");
     }
