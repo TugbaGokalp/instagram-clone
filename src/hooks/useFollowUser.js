b13171c1c1250ed26bd70ffc8acc1c706ac0f0d8
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-19 13:57:07 +0300

    First step of useFollowUser hook

---
+++ src/hooks/useFollowUser.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import useAuthStore from "../store/authStore";
+import useUserProfileStore from "../store/userProfileStore";
+import useShowToast from "./useShowToast";
+import { firestore } from "../firebase/firebase";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+
+const useFollowUser = (userId) => {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const { authUser, setAuthUser } = useAuthStore();
+  const { userProfile, setUserProfile } = useUserProfileStore();
+  const showToast = useShowToast();
+
+  const handleFollowUser = async () => {
+    setIsUpdating(true);
+    try {
+      const currentUserRef = doc(firestore, "users", authUser.uid);
+      const userToFollowOrUnfollorRef = doc(firestore, "users", userId);
+      await updateDoc(currentUserRef, {
+        following: isFollowing ? arrayRemove(userId) : arrayUnion(userId),
+      });
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  useEffect(() => {
+    if (authUser) {
+      const isFollowing = authUser.following.includes(userId);
+      setIsFollowing(isFollowing);
+    }
+  }, [authUser, userId]);
+
+  return { isUpdating, isFollowing, handleFollowUser };
+};
+
+export default useFollowUser;
