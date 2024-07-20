e292ff273488c9d2d28330c4b4665d86a16470e5
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-20 08:08:24 +0300

    Moved Profile as component

--- src/components/Sidebar/SidebarItems.jsx
+++ src/components/Sidebar/SidebarItems.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import Notifications from "./Notifications";
+import ProfileLink from "./ProfileLink";
 
 const SidebarItems = () => {
   return (
@@ -7,6 +8,7 @@ const SidebarItems = () => {
       <Home />
       {/* Search */}
       <Notifications />
+      <ProfileLink />
     </>
   );
 };
