b8f80b6995a1a2a6db91f815bbbb2b0c6f322815
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-07-24 21:05:55 +0300

    Created useCreatePost hook

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -25,12 +25,23 @@ import useAuthStore from "../../store/authStore";
 import usePostStore from "../../store/postStore";
 import useUserProfileStore from "../../store/userProfileStore";
 import { useLocation } from "react-router-dom";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { firestore, storage } from "../../firebase/firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const CreatePost = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [caption, setCaption] = useState("");
   const imageRef = useRef(null);
   const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImg();
+  const { isLoading, handleCreatePost } = useCreatePost();
+  const showToast = useShowToast();
 
   return (
     <>
@@ -125,6 +136,7 @@ function useCreatePost() {
   const authUser = useAuthStore((state) => state.user);
   const createPost = usePostStore((state) => state.createPost);
   const addPost = useUserProfileStore((state) => state.addPost);
+  const userProfile = useUserProfileStore((state) => state.userProfile);
   const { pathname } = useLocation();
 
   const handleCreatePost = async (selectedFile, caption) => {
@@ -137,6 +149,32 @@ function useCreatePost() {
       createdAt: Date.now(),
       createdBy: authUser.uid,
     };
+
+    try {
+      const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+      const userDocRef = doc(firestore, "users", authUser.uid);
+      const imageRef = ref(storage, `posts/${postDocRef.id}`);
+
+      await updateDoc(userDocRef, { posts: arrayUnion(postDocRef.id) });
+      await uploadString(imageRef, selectedFile, "data_url");
+      const downloadURL = await getDownloadURL(imageRef);
+
+      await updateDoc(postDocRef, { imageURL: downloadURL });
+
+      newPost.imageURL = downloadURL;
+
+      if (userProfile.uid === authUser.uid)
+        createPost({ ...newPost, id: postDocRef.id });
+
+      if (pathname !== "/" && userProfile.uid === authUser.uid)
+        addPost({ ...newPost, id: postDocRef.id });
+
+      showToast("Success", "Post created successfully", "success");
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return { isLoading, handleCreatePost };
