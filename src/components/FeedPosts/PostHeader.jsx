f4378158eda86c2d83e61976747a57000708c2f5
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-08-10 18:34:38 +0300

    added follow-unfollow functionality in to feed posts

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,48 +1,51 @@
-import { Avatar, Box, Flex, SkeletonCircle, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Flex, SkeletonCircle } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import useFollowUser from "../../hooks/useFollowUser";
 
 const PostHeader = ({ post, creatorProfile }) => {
-  console.log("creatorProfile", creatorProfile);
+  const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(
+    post.createdBy
+  );
   return (
-    <Flex
-      justifyContent={"flex-start"}
-      alignItems={"center"}
-      w={"full"}
-      my={2}
-      gap={2}
-    >
-      {creatorProfile ? (
-        <Link to={`/${creatorProfile.username}`}>
-          <Avatar
-            src={creatorProfile.profilePicURL}
-            alt="user profile pic"
-            size={"sm"}
-          />
-        </Link>
-      ) : (
-        <SkeletonCircle size={10} />
-      )}
-
-      <Flex fontSize={12} fontWeight={"bold"} gap={2}>
+    <Flex justifyContent={"flex-start"} alignItems={"center"} w={"full"} my={2}>
+      <Flex alignItems={"center"} gap={2}>
         {creatorProfile ? (
           <Link to={`/${creatorProfile.username}`}>
-            {creatorProfile.username}
+            <Avatar
+              src={creatorProfile.profilePicURL}
+              alt="user profile pic"
+              size={"sm"}
+            />
           </Link>
         ) : (
-          <SkeletonCircle w={"100px"} h={"10px"} />
+          <SkeletonCircle size={10} />
         )}
-        <Box color={"gray.500"}>•1w</Box>
+
+        <Flex fontSize={12} fontWeight={"bold"} gap={2}>
+          {creatorProfile ? (
+            <Link to={`/${creatorProfile.username}`}>
+              {creatorProfile.username}
+            </Link>
+          ) : (
+            <SkeletonCircle w={"100px"} h={"10px"} />
+          )}
+          <Box color={"gray.500"}>•1w</Box>
+        </Flex>
       </Flex>
       <Box cursor={"pointer"}>
-        <Text
+        <Button
+          size={"xs"}
+          bg={"transparent"}
           fontSize={12}
           color={"blue.500"}
           fontWeight={"bold"}
           _hover={{ color: "white" }}
           transition={"0.2s ease-in-out"}
+          onClick={handleFollowUser}
+          isLoading={isUpdating}
         >
-          Unfollow
-        </Text>
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
       </Box>
     </Flex>
   );
