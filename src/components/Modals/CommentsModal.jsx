8335a9b4fedee7c04adf0e2887d6583f1bde5898
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-08-11 13:46:48 +0300

    added ability to comment from modal

--- src/components/Modals/CommentsModal.jsx
+++ src/components/Modals/CommentsModal.jsx
@@ -10,8 +10,31 @@ import {
   ModalOverlay,
 } from "@chakra-ui/react";
 import Comment from "../Comment/Comment";
+import usePostComment from "../../hooks/usePostComment";
+import { useEffect, useRef } from "react";
 
 const CommentsModal = ({ isOpen, onClose, post }) => {
+  const { handlePostComment, isCommenting } = usePostComment();
+  const commentRef = useRef(null);
+  const commentsContainerRef = useRef(null);
+  const handleSubmitComment = async (e) => {
+    e.preventDefault();
+    await handlePostComment(post.id, commentRef.current.value);
+    commentRef.current.value = "";
+  };
+
+  useEffect(() => {
+    const scrollToBottom = () => {
+      commentsContainerRef.current.scrollTop =
+        commentsContainerRef.current.scrollHeight;
+    };
+    if (isOpen) {
+      setTimeout(() => {
+        scrollToBottom();
+      }, 100);
+    }
+  }, [isOpen, post.comments.length]);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInLeft">
       <ModalOverlay />
@@ -25,15 +48,22 @@ const CommentsModal = ({ isOpen, onClose, post }) => {
             flexDir={"column"}
             maxH={"250px"}
             overflowY={"auto"}
+            ref={commentsContainerRef}
           >
             {post.comments.map((comment, idx) => (
               <Comment key={idx} comment={comment} />
             ))}
           </Flex>
-          <form style={{ marginTop: "2rem" }}>
-            <Input placeholder="Comment" size={"sm"} />
+          <form onSubmit={handleSubmitComment} style={{ marginTop: "2rem" }}>
+            <Input placeholder="Comment" size={"sm"} ref={commentRef} />
             <Flex w={"full"} justifyContent={"flex-end"}>
-              <Button type="submit" ml={"auto"} size={"sm"} my={4}>
+              <Button
+                type="submit"
+                ml={"auto"}
+                size={"sm"}
+                my={4}
+                isLoading={isCommenting}
+              >
                 Post
               </Button>
             </Flex>
