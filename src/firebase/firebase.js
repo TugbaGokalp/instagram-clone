6fd199842ab9030ff4316498511fdabe8d4ef356
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-08-11 14:41:18 +0300

    added .env file for firebase security

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlNjLp4WLxT0GDJ8B_rJS4tHF55Rz7Nm0",
-  authDomain: "insta-clone-yt-58575.firebaseapp.com",
-  projectId: "insta-clone-yt-58575",
-  storageBucket: "insta-clone-yt-58575.appspot.com",
-  messagingSenderId: "369347178386",
-  appId: "1:369347178386:web:9756559165b423d8f01b4b",
-  measurementId: "G-G6Z2GPDEG2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
